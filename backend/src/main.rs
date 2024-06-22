669704c99224505a590d7e1958e48939bcccf0a7
commonkestrel <jettb6@gmail.com>
2024-06-21 17:56:07 -0700

    Simple `actix_web` webserver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware::Logger, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use env_logger::Env;
+use std::path::{Path, PathBuf};
+
+const FRONTEND: &str = env!("FONT_FRONTEND");
+
+#[get("/")]
+async fn index() -> actix_web::Result<NamedFile> {
+    let path = Path::new(FRONTEND).join("index.html");
+    Ok(NamedFile::open(path)?)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    HttpServer::new(|| 
+        App::new()
+            .service(index)
+            .service(actix_files::Files::new("/static", Path::new(FRONTEND).join("static")))
+            .service(actix_files::Files::new("/js", Path::new(FRONTEND).join("out")))
+            .service(actix_files::Files::new("/css", Path::new(FRONTEND).join("css")))
+            .wrap(Logger::default())
+    )
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
